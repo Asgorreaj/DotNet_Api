3ba1e20b2a112b88a930afcd92358fa44de0bfd6
MD. Asgor Hossain Reaj <asgorreaj@gmail.com>
2023-11-13 08:39:49 +0600

    create a api and call a postman

---
+++ api create/Controllers/DemoController.cs
@@ -0,0 +1,42 @@
+﻿using api_create.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.UI.WebControls;
+
+namespace api_create.Controllers
+{
+    public class DemoController : ApiController
+    {
+        [HttpGet]
+        [Route("api/names")]
+        public HttpResponseMessage Get()
+        {
+            var names = new string[] { "Asgor", "Reaj" };
+            return Request.CreateResponse(HttpStatusCode.OK, names);
+        }
+        [HttpGet]
+        [Route("api/name/")]
+        public HttpResponseMessage GetName() 
+        {
+            return Request.CreateResponse(HttpStatusCode.OK, "Ajgor");
+        }
+        [HttpGet]
+        [Route("api/name/{st_id}")]
+        public HttpResponseMessage GetName(int st_id)
+        {
+            string name = st_id == 1 ? "Asgor" : "not recognize";
+            return Request.CreateResponse(HttpStatusCode.OK, name);
+        }
+        [HttpPost]
+        [Route("api/test/post")]
+        public HttpResponseMessage TestPost(Models.Login u)
+        {
+            return Request.CreateResponse(HttpStatusCode.OK,u.Uname);
+        }
+
+    }
+}
---
+++ api create/Models/Login.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace api_create.Models
+{
+    public class Login
+    {
+        public string Uname {  get; set; }
+        public string Password {  get; set; }
+    }
+}
